dd953567dd086a3fb891ceaa7ba5185cb2d72b45
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-02 04:00:06 +0100

    update endpoints

--- server/api/[user]/[slug].ts
+++ server/api/[user]/[slug].ts
@@ -1,6 +1,6 @@
 export default defineEventHandler (async (event) => {
     let slug = String(getRouterParam(event, 'slug'))
     let user = String(getRouterParam(event, 'user'))
-    const response = await $fetch('http://localhost/api/'+user+'/'+slug);
+    const response = await $fetch('http://localhost/api/page/'+user+'/'+slug);
     return response.data;
 })
