124797005f1c3c6d2e0ddbd237c0c201a37f94ac
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-02 22:43:22 +0100

    add logo image

--- components/Header/Main.vue
+++ components/Header/Main.vue
@@ -3,8 +3,14 @@
         <div class="container-fluid">
         <div class="header">
             <div class="row">
-                <div class="col-lg-6"> 
-                    <h1 style="color:white;">Logo</h1>
+                <div class="col-lg-6">
+                  <ClientOnly>
+                    <div class="logo" :style="{ 'background-image': 'url(/images/site/logo/'+logo+'' }"> </div>
+                    <template #fallback>
+                      <!-- this will be rendered on server side -->
+                      <span class="loader"></span>>
+                    </template>
+                  </ClientOnly>
                 </div>
                 <div class="col-lg-6">
                     <ButtonsListHeader :buttons="props.buttons"/>
@@ -20,6 +26,24 @@ const props = defineProps({
   buttons: {
     type: Array,
     required:false
+  },
+  logo:{
+    type:String,
+    required:false
   }
 })
 </script>
+
+<style scoped lang="scss">
+.logo {
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 13px;
+  width: 4rem;
+  height: 5rem;
+  float:left;
+  position:relative;
+  animation: fadeIn 1.5s;
+}
+</style>
