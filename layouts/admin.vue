907818b4184a02530b6dfa153ec75ffa4f30e322
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-07 23:43:34 +0100

    show button if user is authenticated

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -2,14 +2,15 @@
     <div>
         <div style="border:1px solid lightgray;padding:20px 20px;">
           <h3 style="color:lightgray">CV Maker</h3>
-          <a @click="logout">Logout</a>
+          <a @click="logout" v-if="isAuthenticated">Logout</a>
         </div>
       <br><br>
         <slot />
     </div>
 </template>
 <script setup>
-
+const user = useSanctumUser()
+const isAuthenticated = !!user.value
 const logout = async () => {
   const { logout } = useSanctumAuth();
   await logout();
