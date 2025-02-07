d30d0f96046c9b8ea302d05b1b7c066ac35e4cdf
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-08 00:04:52 +0100

    show button if user is authenticated - styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
-      <div style="border:1px solid lightgray;padding:20px 20px;padding-bottom: 62px;padding-top: 19px;">
-        <h3 style="color:lightgray;float:left;">CV Maker</h3>
-        <a style="float:right;margin-bottom:20px;float:right;" href="admin/login">login</a>
+      <div style="
+          border:1px solid lightgray;
+          padding:20px 20px;
+          display:flex;
+          flex-direction:row;
+          justify-content:space-between;
+          align-items: center;"
+      >
+        <h3 style="color:lightgray;margin:0">CV Maker</h3>
+        <a style="font-weight: bold;color:black;margin-bottom:20px;float:right;text-decoration: none;" href="admin/login">login</a>
       </div>
       <br><br>
         <h1>Company Logo</h1>
