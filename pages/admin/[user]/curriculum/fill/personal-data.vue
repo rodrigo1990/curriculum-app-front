cf7a77eb373b72b7494aee052f6cc8fb54559670
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-20 21:36:52 +0100

    include birthday calendar in personal data panel

--- pages/admin/[user]/curriculum/fill/personal-data.vue
+++ pages/admin/[user]/curriculum/fill/personal-data.vue
@@ -8,7 +8,16 @@
       <Field name="surname" type="surname"  class="form-control" placeholder="Surname" :rules="validateName"/>
       <ErrorMessage name="surname" />
       <br>
-      <input type="text" class="form-control" placeholder="Birthday" />
+      <div class="calendar-box" ref="calendarBox">
+        <span v-show="showCalendar">
+          <VDatePicker v-model="birthday" style="position:absolute;" />
+        </span>
+        <Field name="birthday" type="birthday" class="form-control" placeholder="Birthday"
+               v-model="formattedBirthday"
+               @click="showCalendar = !showCalendar"
+               :rules="validateBirthday" />
+        <ErrorMessage name="birthday" />
+      </div>
       <br>
       <button class="btn btn-primary">Next</button>
 <!--      <NuxtLink :to="'/admin/'+$route.params.user+'/curriculum/fill/profile-picture'"-->
@@ -19,6 +28,9 @@
   </div>
 </template>
 <script setup>
+  const birthday = ref(new Date());
+  const showCalendar = ref(false);
+  const calendarBox = ref(null);
   const onSubmit = (values) => {
     console.log(values);
   }
@@ -29,8 +41,48 @@
     return regex.test(name) ? true : 'This field must contain only letters and spaces';
   };
 
+  const validateBirthday = (date) => {
+    const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    if(!date) return 'This field is required';
+    return regex.test(date) ? true : 'This field must be a date in the format dd/mm/yyyy';
+  };
+
+
+  const handleClickOutside = (event) => {
+    if (calendarBox.value && !calendarBox.value.contains(event.target)) {
+      showCalendar.value = false;
+    }
+  }
+
+  const formatDate = (date) => {
+    const dateObj = new Date(date);
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const year = dateObj.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  const formattedBirthday = computed(() => {
+    const date = new Date();
+    if(birthday.value) {
+      if(formatDate(birthday.value) === formatDate(date))
+        return '';
+      else
+        return formatDate(birthday.value)
+    }
+  });
+
+
+  onMounted(() => {
+    document.addEventListener('click', handleClickOutside);
+  });
+
+  onBeforeUnmount(() => {
+    document.removeEventListener('click', handleClickOutside);
+  });
 
 </script>
 <style scoped lang="scss">
 @import url("~/assets/styles/admin/curriculum/fill/personal-data.scss");
+@import url("~/assets/styles/calendar-box.scss");
 </style>
