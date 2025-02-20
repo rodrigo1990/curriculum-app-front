614413fbf20364065515a7cab175fd42b640f506
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-20 20:01:45 +0100

    validate personal data

--- pages/admin/[user]/curriculum/fill/personal-data.vue
+++ pages/admin/[user]/curriculum/fill/personal-data.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="panel">
     <h2>Personal data</h2>
-    <input type="text" class="form-control" placeholder="Name" />
-    <br>
-    <input type="text" class="form-control" placeholder="Surname" />
-    <br>
-    <input type="text" class="form-control" placeholder="Birthday" />
-    <br>
-    <NuxtLink :to="'/admin/'+$route.params.user+'/curriculum/fill/profile-picture'"
-              class="btn btn-primary">
-      Next
-    </NuxtLink>
-
+    <Form novalidate @submit="onSubmit">
+      <Field name="name" type="name" class="form-control" placeholder="Name" :rules="validateName"  />
+      <ErrorMessage name="name" />
+      <br>
+      <Field name="surname" type="surname"  class="form-control" placeholder="Surname" :rules="validateName"/>
+      <ErrorMessage name="surname" />
+      <br>
+      <input type="text" class="form-control" placeholder="Birthday" />
+      <br>
+      <button class="btn btn-primary">Next</button>
+<!--      <NuxtLink :to="'/admin/'+$route.params.user+'/curriculum/fill/profile-picture'"-->
+<!--                class="btn btn-primary">-->
+<!--        Next-->
+<!--      </NuxtLink>-->
+    </Form>
   </div>
 </template>
 <script setup>
+  const onSubmit = (values) => {
+    console.log(values);
+  }
+
+  const validateName = (name) => {
+    if(!name) return 'This field is required';
+    const regex = /^[A-Za-z\s]+$/;
+    return regex.test(name) ? true : 'This field must contain only letters and spaces';
+  };
+
+
 </script>
 <style scoped lang="scss">
 @import url("~/assets/styles/admin/curriculum/fill/personal-data.scss");
