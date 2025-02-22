955b7023047b2fc420be97bb3ee40b92dd1f95ae
rodrigo1990 <mcd77.1990@gmail.com>
2025-02-22 05:02:57 +0100

    refactor input calendar to component

--- pages/admin/[user]/curriculum/fill/personal-data.vue
+++ pages/admin/[user]/curriculum/fill/personal-data.vue
@@ -14,16 +14,7 @@
       <Field name="telephone" type="telephone"  class="form-control" placeholder="Telephone" />
       <ErrorMessage name="telephone" />
       <br>
-      <div class="calendar-box" ref="calendarBox">
-        <span v-show="showCalendar">
-          <VDatePicker v-model="birthday" style="position:absolute;" />
-        </span>
-        <Field name="birthday" type="birthday" class="form-control" placeholder="Birthday"
-               v-model="formattedBirthday"
-               @click="showCalendar = !showCalendar"
-               :rules="validateBirthday" />
-        <ErrorMessage name="birthday" />
-      </div>
+      <InputCalendar />
       <br>
       <button class="btn btn-primary">Next</button>
     </Form>
@@ -31,9 +22,7 @@
 </template>
 <script setup>
   const route = useRoute()
-  const birthday = ref(new Date());
-  const showCalendar = ref(false);
-  const calendarBox = ref(null);
+
   const client = useSanctumClient();
   const onSubmit = async (values) => {
     await client('/api/admin/curriculum/fill-personal-data', {
@@ -58,11 +47,7 @@
     return regex.test(name) ? true : 'This field must contain only letters and spaces';
   };
 
-  const validateBirthday = (date) => {
-    const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
-    if(!date) return 'This field is required';
-    return regex.test(date) ? true : 'This field must be a date in the format dd/mm/yyyy';
-  };
+
 
   const validateEmail = (email) => {
     if(!email) return 'This field is required';
@@ -71,38 +56,7 @@
   };
 
 
-  const handleClickOutside = (event) => {
-    if (calendarBox.value && !calendarBox.value.contains(event.target)) {
-      showCalendar.value = false;
-    }
-  }
 
-  const formatDate = (date) => {
-    const dateObj = new Date(date);
-    const day = String(dateObj.getDate()).padStart(2, '0');
-    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
-    const year = dateObj.getFullYear();
-    return `${day}/${month}/${year}`;
-  };
-
-  const formattedBirthday = computed(() => {
-    const date = new Date();
-    if(birthday.value) {
-      if(formatDate(birthday.value) === formatDate(date))
-        return '';
-      else
-        return formatDate(birthday.value)
-    }
-  });
-
-
-  onMounted(() => {
-    document.addEventListener('click', handleClickOutside);
-  });
-
-  onBeforeUnmount(() => {
-    document.removeEventListener('click', handleClickOutside);
-  });
 
 </script>
 <style scoped lang="scss">
